b28412557aa0eb3eb6e0e13a09e74375d0527eae
rostyslav-bilous <rsbil256@gmail.com>
2026-05-21 16:37:46 +0200

    Added a leafmap.folium interactive map.

--- src/viz/pages/Home.py
+++ src/viz/pages/Home.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import leafmap.foliumap as leafmap
 
 st.title("🏠 Home")
+m = leafmap.Map(center=[52.5, -2.0  ], zoom=6.5)
+m.add_basemap('CartoDB.Positron')
+m.to_streamlit()
 
